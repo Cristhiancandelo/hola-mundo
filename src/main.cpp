066693d8ea7995f2ca49d961fdbdcdc1ae26a24e
christhian candelo <christhian candelo>
2016-03-27 17:39:30 -0500

    Esta es mi modificacion

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,12 @@
 using namespace std;
 
 int main() {
+	int n, m;
+   
     cout << "!!!Hello World Cristhian!!!" << endl; // prints !!!Hello World!!!
+    cout <<"ingrese n"; cin>> n;
+    cout <<"ingrese m"; cin>> m;
+    	int resultado = n+ m;
+    cout << "Su resultado es "<<resultado<<endl;
     return 0;
 }
